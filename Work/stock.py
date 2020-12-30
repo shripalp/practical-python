86398b8d4a0931a4756755b7dc04c736197085a1
Shripal <shripalp@gmail.com>
2020-12-30 09:56:33 -0700

    update

--- Work/stock.py
+++ Work/stock.py
@@ -1,32 +1,32 @@
 class stock:
-    __slots__ = ( 'name', '_shares', 'price' )
+    __slots__ = ( 'name', '_shares', 'price' ) #attributes cannot be changed or added
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
     
-    @property
+    @property #creating a function shares
     def shares(self):
         return self._shares
 
     @shares.setter
     def shares(self, value):
-        if not isinstance(value, int):
+        if not isinstance(value, int): # this function checks the value validation
             raise TypeError ('Expected int')
         self._shares = value
         
     def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
 
-    @property    
+    @property    #function can be called without ()
     def cost(self):
         return self.shares * self.price
 
-    @property
+    @property  #function can be called with out ()
     def sell(self, n):
         self.shares = self.shares - n
 
-class mystock(stock):
+class mystock(stock): #child class(inheritance) from class stock
     def panic(self):
         self.sell(self.shares)
     def cost(self):
